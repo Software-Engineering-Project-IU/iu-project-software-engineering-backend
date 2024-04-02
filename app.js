10c3ec95dc9a49ba902a5fe5f6d34d8fef97c3a6
dieKrake <kevin.krazius@iubh-fernstudium.de>
2024-04-02 12:44:51 +0200

    added testdata, tested request with postman

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
-const express = require('express');
+/*
+ *	app.js
+ *
+ *	Ersteller:		    Kevin Krazius
+ *	Erstellungsdatum:	03-29-2024
+ *	Info/Notizen:		  Express-App, definiert das Backend
+ *
+ *	Editiert von:
+ *	Editiert am:
+ *	Info/Notizen:
+ *
+ */
+
+const userData = require("./src/database/userData.json");
+const quizData = require("./src/database/quizData.json");
+
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hallo Welt!');
+// Endpoint zum Abrufen der Benutzerdaten
+app.get("/users", (req, res) => {
+  res.json(userData);
 });
 
+// Endpoint zum Abrufen der Quizdaten
+app.get("/quiz", (req, res) => {
+  res.json(quizData);
+});
+
+// Starten des Servers
 app.listen(port, () => {
   console.log(`Server läuft auf http://localhost:${port}`);
 });
